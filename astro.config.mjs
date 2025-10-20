b3b3edbe0380c53552236c97140d2bdd6c40c9d8
Ken Patrick Garcia <kenpatrickgarcia123@gmail.com>
2025-10-20 22:55:05 +0800

    feat: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: false,
-  }),
+  adapter: vercel(),
 });
